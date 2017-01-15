f935cad86fd8df574b73814c2d4c767d2407d339
asalilje <asa.lilje@gmail.com>
2017-01-15 23:10:07 +0100

    More terse syntax

--- app/templates/redux/FilterLink.js
+++ app/templates/redux/FilterLink.js
@@ -1,38 +1,19 @@
-import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Link from './Link';
 import * as todoFilterActions from './actions/todoFilterActions';
-import { todoFilterOptions } from './reducers/todoFilter';
 
-class FilterLink extends React.Component {
-    render() {
-        const props = this.props;
-        return (
-            <Link active={props.active}
-                onClick={props.onClick}>
-                {props.children}
-            </Link>
-        );
+const mapStateToProps = (state, ownProps) => ({
+    active: ownProps.filter === state.todoFilter
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    onClick() {
+      dispatch(todoFilterActions.setFilter(ownProps.filter));
     }
-}
+});
 
-FilterLink.propTypes = {
-    currentFilter: PropTypes.string,
-    selectedFilter: PropTypes.string,
-    children: PropTypes.string,
-    onClick: PropTypes.func
-};
+const FilterLink = connect(
+  mapStateToProps, mapDispatchToProps
+)(Link);
 
-const mapStateToProps = (state, ownProps) => {
-    return {
-        active: ownProps.filter === state.todoFilter
-    };
-};
-
-const mapDispatchToProps = (dispatch, ownProps) => {
-    return {
-        onClick: filter => dispatch(todoFilterActions.setFilter(ownProps.filter))     
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(FilterLink);
+export default FilterLink;
